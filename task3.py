8cbf0496d88b627d667b7ead2461616bbf6b5fd8
Vladimir Bulychev <bulychev.v.m@gmail.com>
2025-01-19 20:17:30 +0700

    DZ

--- task3.py
+++ task3.py
@@ -6,3 +6,30 @@
 # слово2: количество
 #
 # Убедитесь, что слова записаны в алфавитном порядке.
+
+with open("task3.txt", encoding="utf-8") as f:
+    string = f.read()
+
+string = string.lower()
+
+if "—" in string:
+    string = string.replace("—", "")
+if "." in string:
+    string = string.replace(".", '')
+if "," in string:
+    string = string.replace(",", '')
+
+words = string.split()
+words_formated = list(set(words))
+ans = open("task3Answer.txt", 'w', encoding="utf-8")
+Ans = []
+for i in range(0, len(words_formated)):
+    Ans.append(str(f"{words_formated[i]}: {words.count(words_formated[i])}\n"))
+
+Ans.sort()
+for j in range(0, len(Ans)):
+    ans.write(Ans[j])
+print(words)
+
+
+
--- task5.py
+++ task5.py
@@ -5,3 +5,24 @@
 # Его длина: длина
 #
 # Выведите это слово и длину в консоль.
+
+with open("task5.txt", encoding="utf-8") as f:
+    string = f.read()
+
+string = string.lower()
+if "—" in string:
+    string = string.replace("—", "")
+if "." in string:
+    string = string.replace(".", '')
+if "," in string:
+    string = string.replace(",", '')
+if "!" in string:
+        string = string.replace("!", '')
+words = string.split()
+lens = []
+for i in range(0, len(words)):
+    lens.append(len(words[i]))
+Key = lens.index(max(lens))
+
+with open("task5Answer.txt", "w", encoding="utf-8") as f:
+    f.write(f"Самое длинное слово: {words[Key]}\nЕго длина: {len(words[Key])}")
